6ccc5b7d5a84381ec48e031dde0ee1368fd80317
Ajay <ajay.ajg183@gmail.com>
2020-06-10 15:50:14 +0530

    Initial commit

---
+++ app/src/main/java/com/astro/astrotbbt/MainActivity.java
@@ -0,0 +1,21 @@
+package com.astro.astrotbbt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void play(View view){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.whip);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
